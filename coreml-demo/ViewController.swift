20ddb1b3b781bbac8fcc68a6ae43de04b2e8ee32
tgz <qiu6288@gmail.com>
2018-01-17 15:18:14 +0800

    demo worked!

--- coreml-demo/ViewController.swift
+++ coreml-demo/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var topImage: UIImageView!
+    @IBOutlet weak var bottomImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func runButtonTouched(_ sender: Any) {
+        guard let image = UIImage(named: "12.jpg") else {return}
+        topImage.image = image
+        NSLog("model load start")
+        let m = dped()
+        NSLog("model loaded, start mock data")
+        guard let inputData = mockData() else { return }
+        NSLog("start prediction")
+        if let pb = image.pixelBuffer(width: 1024, height: 768) {
+            let r = try? m.prediction(input__0: pb)
+            let i = r?.generator__output__0.image(offset: 0, scale: 255)
+            bottomImage.image = i
+        }
+        
+        NSLog("prediction finished")
+    }
 
+    func mockData() -> MLMultiArray? {
+        guard var array = try? MLMultiArray(shape: [3, 768, 1024], dataType: MLMultiArrayDataType.double) else { return nil }
+        for i in 0..<3*768*1024 {
+            
+            array[i] = NSNumber(value: Double(arc4random() % 255) / 255.0)
+        }
+        return array
+    }
 }
 
